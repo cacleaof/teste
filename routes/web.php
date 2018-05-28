c1a1a0e997ad193fe1bb00e6e91e3810baa60379
Carlos Leao <cacleaof@gmail.com>
2018-05-27 21:21:00 -0300

    Erro consertado

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+$this->group(['middleware' => ['auth'], 'namespace' => 'Admin', 'prefix' => 'admin'], function(){
+	$this->get('balance', 'BalanceController@index')->name('AdminBalance');
+	$this->get('/', 'AdminController@index')->name('admin.home');
 });
+
+$this->get('/', 'SiteController@index')->name('home');
+
+Auth::routes();
+
